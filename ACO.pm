8f114047c1b6e42f09b87a46230459dcd073889a
Owen Dyckhoff <owen.dyckhoff@gmail.com>
2013-04-30 12:03:47 +0100

    Defined conditions for critical parameters

--- ACO.pm
+++ ACO.pm
@@ -6,4 +6,13 @@ package ACO;
 use strict;
 use warnings;
 
+sub get_main_params {
+    my ( $alpha, $beta, $rho ) = @_;
 
+    return if ($alpha < 0 || $beta < 0); # α, β : R > 0
+    return if ($rho   < 0 || $rho  > 1); #    ρ : [0, 1]
+
+   
+
+     
+}
